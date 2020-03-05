ae6f95d15cda2d8b355378288df7a3a29077eaf1
Idole Koun <idole.koun@outlook.com>
2020-03-05 18:12:12 -0500

    Add new file

---
+++ RandomSpherePacking/Sphere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Sphere
+{
+    [SerializeField] private Vector3 centerpoint;
+    [SerializeField] private float radius;
+
+    public Sphere(Vector3 centerpoint, float radius)
+    {
+        this.centerpoint = centerpoint;
+        this.radius = radius;
+    }
+
+    public Vector3 Centerpoint
+    {
+        get { return centerpoint; }
+        set { centerpoint = value; }
+    }
+
+    public float Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+}
